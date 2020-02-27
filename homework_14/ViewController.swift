1d17396e6f7a3ae12523318c426b7e8c8a5b114f
Alexandra Lesnichaya <let93@>
2020-02-27 22:06:39 +0300

    Initial Commit

---
+++ homework_14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework_14
+//
+//  Created by Александра Лесничая on 2/27/20.
+//  Copyright © 2020 Alexandra Lesnichaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
